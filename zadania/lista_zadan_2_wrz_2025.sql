3a216c47522ac747d46c0fb7a9f4b0126fff3fe0
Igor <151391534+i120ms@users.noreply.github.com>
2025-10-06 10:02:51 +0200

    Create lista_zadan_2_wrz_2025.sql

---
+++ zadania/lista_zadan_2_wrz_2025.sql
@@ -0,0 +1,13 @@
+Zad. 1. Przypisz Polskę, Słowację, Czechy, Słowenię i Węgry do nowego regionu: Central Europe.
+Zad. 2. Na Wyspach Heard i McDonalda zamieszkało dwoje ludzi. Uwzględnij to w bazie danych (zmień populację i oczekiwaną długość życia).
+Zad. 3. Wszystkie kraje, gdzie królową jest Elżbieta II (Elisabeth II) i które nie są niepodległe (IndepYear IS NULL) ogłosiły niepodległość w roku 2025. Zaznacz to w bazie danych.
+Zad. 4. W Stanach Zjednoczonych procent populacji mówiącej po angielsku spadł do 80 %. Zaktualizuj bazę danych.
+Zad. 5. W Angoli języki: Mbundu i Ovimbundu uzyskały tytuły języków oficjalnych. Uwzględnij to bazie w danych.
+Zad. 6. Chiny, Hong Kong i Macao utworzyły nowy region: Hundred Acre Wood. Uwzględnij to w bazie danych. Dodatkowo zmień rządzącego całym regionem na: Winnie-the-Pooh.
+Zad. 7. Uwzględnij w bazie następujące zmiany dotyczące Federacji Rosyjskiej:
+i) nazwa: Mordor,
+ii) populacja: 90 % obecnej,
+iii) oczekiwana długość życia: o 10 mniejsza,
+iv) władca: Sauron.
+Zad. 8. Zwiększ ID każdego z miast o 100.
+Zad. 9. Dla całego świata: podwój wartość GNP. Zachowaj poprzednią wartość GNP w GNPOld.
