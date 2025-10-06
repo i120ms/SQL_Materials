74c80738ceeb796d6e5373c7e3994a2b02f0d857
Igor <151391534+i120ms@users.noreply.github.com>
2025-10-06 10:01:12 +0200

    Update lista_zadan_3_paź_2025.sql

--- zadania/lista_zadan_3_paź_2025.sql
+++ zadania/lista_zadan_3_paź_2025.sql
@@ -1,2 +1,18 @@
+Zad. 1. Wyświetl wszystkie tabele w bazie world.
+Zad. 2. Wyświetl informacje o kolumnach tabeli city.
+Zad. 3. Utwórz nowy kraj: Neverland.
+Zad. 4. Utwórz 2 nowe miasta we Francji.
+Zad. 5. Wymyśl 3 nowe języki i wprowadź je w Argentynie, jako języki nieoficjalne.
+Zad. 6. Stany Zjednoczone skopiowały się. Skopiuj je w bazie (zmień tylko PRIMARY KEY).
+Zad. 7. Skopiuj języki z Argentyny do Urugwaju. Gdy język się powtarza, ustaw procent ludzi w nim mówiących na 98.
+Zad. 8. Usuń Skopiowane Stany Zjednoczone.
+Zad. 9. Usuń wszystkie nieoficjalne języki z Argentyny i Urugwaju.
+Zad. 10. Spróbuj dodać nowe miasto o takim samym ID jak Wrocław. Polecenie napisz tak, aby w przypadku niepowodzenia liczba mieszkańców podwoiła się.
+Zad. 11. Usuń 5 miast o największej populacji.
+Zad. 12. Usuń wszystkie języki.
+Zad. 13. Zaimportuj bazę ponownie.
+Zad. 14.* Dodaj miasto do kraju o najmniejszej populacji.
+Zad. 15.* Usuń 5 najmniej zaludnionych miast ze zbioru 3 państw o największej powierzchni.
+
 1. SHOW TABLES; 
 2. DESCRIBE city;
