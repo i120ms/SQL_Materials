547640ef9d157f9854cbef3677cfc81638a7ab6f
Igor <151391534+i120ms@users.noreply.github.com>
2025-10-27 10:00:07 +0100

    Update lista_zadan_4_paź_2025.sql

--- zadania/lista_zadan_4_paź_2025.sql
+++ zadania/lista_zadan_4_paź_2025.sql
@@ -1,1 +1,6 @@
-
+Zad. 1. Wyświetl dwie kolumny: w jednej powinny znaleźć się nazwy regionów, a w drugiej kontynent, na którym dany region leży.
+Zad. 2. Wyświetl nazwę kraju i nazwę regionu, dla krajów, które leżą w Europie Wschodniej.
+Zad. 3. Wyświetl wszystkie kolumny, które powstaną w wyniku złączenia, które pozwoli wypisać wszystkie kraje, w którym mówi się w języku francuskim.
+Zad. 4. Wyświetl nazwę kraju i jego populację z roku 1997, dla każdego kraju, który w tym roku miał ponad 100 000 000 mieszkańców. Uszereguj wyniki malejącą według populacji.
+Zad. 5. Poprzednie polecenie zmodyfikuj tak, aby wyświetlić także kontynent, na którym leży dane państwo.
+Zad. 6. Wypisz całkowitą powierzchnię dla każdego kontynentu (czyli co najmniej 2 kolumny: powierzchnia i nazwa kontynentu). Posortuj rosnąco według powierzchni.
