14b503b3defe9e1f614049a5b62345bbfeae256b
Igor <151391534+i120ms@users.noreply.github.com>
2025-12-08 10:10:20 +0100

    Create lista_zadan_6_2025.sql

---
+++ zadania/lista_zadan_6_2025.sql
@@ -0,0 +1,35 @@
+Zad. 1. Wyświetl nazwę i national_day każdego europejskiego kraju. W przy-
+padku wartości NULL, wpisz dzisiejszą datę.
+
+Zad. 2. Wyświetl wszystkie kraje, których national_day wypadł przed rokiem
+1970.
+Zad. 3. Wyświetl nazwę kraju, national_day i nową kolumnę age, wartości będą
+zależeć od national_day w następujący sposób:
+  
+(−∞, 1600) - bardzo stary
+[1600, 1800) - stary
+[1800, 1900) - mniej stary
+[1900, 1950) - trochę młody
+[1950, 2000) - młody
+[2000,∞) - bardzo młody 
+∅ - BRAK DANYCH
+  
+Zad. 4. Wyświetl kraje, które mają powierzchnię powyżej światowej średniej.
+Zad. 5. Dla każdego kontynentu wyświetl kraje, które mają powierzchnię powyżej
+średniej dla danego kontynentu.
+Zad. 6. Wyświetl wszystkie kraje, w których nie ma oficjalnego języka.
+
+Zad. 7. Wyświetl liczbę krajów, które przez cały okres pomiarów, miały popu-
+lację powyżej 10 milionów.
+
+Zad. 8. Wyświetl liczbę krajów, które choć w jednym roku, miały populację
+powyżej 10 milionów.
+Zad. 9. Wyświetl kraje, które w roku 2003 miały populację poniżej średniej
+(użyj joina)
+
+Zad. 10. Wyświetl kraje, które w roku 2003 miały populację poniżej średniej
+
+(użyj exists, nie używaj joina)
+
+Zad. 11. Wyświetl liczbę regionów, które mają 10 lub więcej krajów.
+Zad. 12.* Policz medianę powierzchni krajów.
