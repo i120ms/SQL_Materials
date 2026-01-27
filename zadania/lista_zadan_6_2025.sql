f552728e107841fa0d1bd7ed57960fa09e909b2c
Igor <151391534+i120ms@users.noreply.github.com>
2026-01-27 10:58:43 +0100

    Update lista_zadan_6_2025.sql

--- zadania/lista_zadan_6_2025.sql
+++ zadania/lista_zadan_6_2025.sql
@@ -26,7 +26,7 @@ Zad. 12.* Policz medianę powierzchni krajów.
   4. SELECT name, area FROM countries WHERE area > (SELECT AVG(area) FROM countries);
   5. SELECT name, area, (SELECT AVG(area) FROM countries) AS srednia FROM countries WHERE area > (SELECT AVG(area) FROM countries);
   6. SELECT DISTINCT name, official FROM country_languages NATURAL JOIN countries WHERE country_id != ALL(SELECT country_id FROM country_languages WHERE official =1);
-  7.
+  7. SELECT DISTINCT name, official FROM country_languages NATURAL JOIN countries WHERE country_id != ANY(SELECT country_id FROM country_languages WHERE official =1);
   8.
   9.
   10.
